6c4618e44a82278686570826e1e5f4a6f823129d
Elton <68150154+aliyevelton@users.noreply.github.com>
2023-11-28 20:52:05 +0400

    Console project

---
+++ Generic custom list/Generic custom list/CustomList.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing;
+
+namespace Generic_custom_list
+{
+    public class CustomList<T>
+    {
+        public int count { get; set; } = 0;
+        public int capacity { get; set; } = 4;
+        public T[] list = { };
+        //private int _index = 0;
+        public CustomList() 
+        {
+            list = new T[capacity];
+        }
+        public CustomList(int capacity) 
+        { 
+            if(capacity < 1) capacity = 4;
+            this.capacity = capacity;
+            list = new T[capacity];
+        }
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                capacity = capacity * 2;
+                Array.Resize(ref list, capacity);
+            }
+            list[count] = item;
+            count++;
+        }
+
+        public T? Find(Predicate<T> match)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (match(list[i]))
+                {
+                    return list[i];
+                }
+            }
+            return default;
+        }
+        public CustomList<T> FindAll(Predicate<T> match)
+        {
+            CustomList<T> newList = new CustomList<T>() { };
+            foreach (T item in list)
+            {
+                if(match(item))
+                    newList.Add(item);
+            }
+            return newList;
+        }
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < capacity; i++)
+            {
+                if(i <= count - 1)
+                {
+                    T Value = list[i];
+                    if (Value.Equals(value))
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        public bool Exists(Predicate<T> match)
+        {
+            foreach (T item in list)
+            { 
+                if(match(item))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool Remove(T value)
+        {
+            for (int i = 0;i < capacity; i++)
+            {
+                if (value.Equals(list[i]))
+                {
+                    CustomList<T> removedList = new CustomList<T>();
+                    Array.Copy(,)
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ Generic custom list/Generic custom list/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Generic_custom_list;
+CustomList<int> list = new(3);
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+
+//Console.WriteLine(list.FindAll(item => item > 2));
+//list.Find();
+//Console.WriteLine(list.Contains(0));
+
+//Console.WriteLine(list.Exists(item => item >= 6));
